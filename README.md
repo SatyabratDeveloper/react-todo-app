5f602c68ed3650189ddde5ffe7ab67add0b7972e
SatyabratDeveloper <satyabrat.jha@vidyamantra.com>
2023-12-12 20:09:44 +0530

    Todo App added

---
+++ README.md
@@ -0,0 +1,38 @@
+# About The Project
+
+Welcome to the React Todo App. It is a software application designed to help users organize and manage tasks or activities they need to accomplish. I have build it using Context API and Local Storage.
+
+# Built With
+
+The major frameworks/libraries used to built the project:
+
+-   React
+-   JavaScript
+-   Tailwind CSS
+-   Context API
+-   Local Storage
+
+# Getting Started
+
+## Installation
+
+You need to follow these steps to install and setup the Todo App.
+
+1. Clone the repo
+
+    ```
+    https://github.com/SatyabratDeveloper/react-todo-app.git
+    ```
+
+2. Install NPM packages
+
+    ```
+    cd react-todo-app
+
+    npm install
+    ```
+
+3. Start your development server
+    ```
+    npm run dev
+    ```
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useTodoContext } from "../contexts";
+
+function TodoForm() {
+    const [todo, setTodo] = useState("");
+
+    const { addTodo } = useTodoContext();
+
+    const submitTodo = (e) => {
+        e.preventDefault();
+
+        if (!todo) return;
+        addTodo({ todo, completed: false });
+        setTodo("");
+    };
+
+    return (
+        <form className="flex" onSubmit={submitTodo}>
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                onChange={(e) => setTodo(e.target.value)}
+                value={todo}
+            />
+            <button
+                type="submit"
+                className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+            >
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
